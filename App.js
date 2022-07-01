6012d54966f7f311d061d6fa7e3e90a87a92550a
Eduardo <eduardorodriguesanjos@gmail.com>
2022-07-01 13:21:22 -0300

    [fix] useState

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // https://github.com/eduardorodrigues97/maxia-native-app
 
-import { React, useRef } from 'react';
+import { React, useRef, useState } from 'react';
 import {useNetInfo} from "@react-native-community/netinfo";
 import { OrientationLock } from 'expo-screen-orientation';
 import { useScreenOrientationLock } from '@use-expo/screen-orientation';
@@ -11,14 +11,13 @@ var started = false;
 
 export default function App() {
   useScreenOrientationLock(OrientationLock.PORTRAIT_UP);
+  const [url, setUrl] = useState('http://teste.maxia.education/');
   netInfo = useNetInfo();
   webRef = useRef(null);
 
-  if (started === true || netInfo.isConnected) {
-    started = true;
-    if (netInfo.isConnected === true){
-      return myWebView(webRef)
-    }
+  if (netInfo.isConnected === true){
+    return myWebView(webRef, url, setUrl)
   }
+
   return OfflineScreen()
 }
--- components/web-view.js
+++ components/web-view.js
@@ -6,33 +6,28 @@ import * as WebBrowser from 'expo-web-browser';
 import { StyleSheet } from 'react-native';
 import Constants from 'expo-constants';
 
-var webRef =  null;
-var last_uri = 'http://teste.maxia.education/';
-
-export default function myWebView(newWebRef) {
-    webRef = newWebRef;
+export default function myWebView(webRef, url, setUrl) {
+    const onShouldStartLoadWithRequest = function(navigator) {
+        // INTERCEPT PDFs
+        setUrl(navigator.url);
+        if (navigator.url.slice(-4) == '.pdf') {
+            webRef.current.stopLoading(); //Some reference to your WebView to make it stop loading that URL
+            WebBrowser.openBrowserAsync(navigator.url)
+            return false;
+        }
+    
+        return true;
+    }
     
     return (
         <WebView 
         ref={webRef}
         style={styles.container}
-        source={{ uri: last_uri }}
+        source={{ uri: url }}
         onShouldStartLoadWithRequest={onShouldStartLoadWithRequest} //for iOS
         onNavigationStateChange ={onShouldStartLoadWithRequest} //for Android
         />
     )
-}
-
-const onShouldStartLoadWithRequest = function(navigator) {
-    // INTERCEPT PDFs
-    last_uri = navigator.url
-    if (navigator.url.slice(-4) == '.pdf') {
-        webRef.current.stopLoading(); //Some reference to your WebView to make it stop loading that URL
-        WebBrowser.openBrowserAsync(navigator.url)
-        return false;
-    }
-
-    return true;
 }
 
 const styles = StyleSheet.create({

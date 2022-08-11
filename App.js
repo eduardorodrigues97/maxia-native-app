671f78879318c513b924c3d9e8327059546b908e
Eduardo <eduardorodriguesanjos@gmail.com>
2022-08-11 15:13:21 -0300

    [feat] splash screen ok

--- App.js
+++ App.js
@@ -1,6 +1,6 @@
 // https://github.com/eduardorodrigues97/maxia-native-app
 
-import { React, useRef, useState } from 'react';
+import { React, useRef, useState, useEffect } from 'react';
 import { StyleSheet } from 'react-native';
 
 // Component imports
@@ -8,20 +8,21 @@ import { WebView } from 'react-native-webview';
 import * as WebBrowser from 'expo-web-browser';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import { StatusBar } from 'expo-status-bar';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Keep the splash screen visible while we fetch resources
+SplashScreen.preventAutoHideAsync();
 
 export default App = () => {
     // Define refs
     const webRef = useRef(null);
     const [url, setUrl] = useState('http://teste.maxia.education/');
     const [statusBarBackgroundColor, setStatusBarBackgroundColor] = useState('#F2F2F2');
+    const [started, setStarted] = useState(0);
 
     // Helper functions
     // URL change handler
     const onShouldStartLoadWithRequest = function (navigator) {
-        // this.canGoBack = navigator.canGoBack;
-        // Set new URL to state
-        // console.log(navigator.url)
-
         // INTERCEPT PDFs
         if (navigator.url.slice(-4) === '.pdf') {
             // setUrl(navigator.url);
@@ -36,20 +37,22 @@ export default App = () => {
             return false;
         }
 
-        // if (navigator.url === 'http://teste.maxia.education/users/sign_in' && url === 'http://teste.maxia.education/') {
-        //     webRef.current.stopLoading(); //Some reference to your WebView to make it stop loading that URL
-        //     setIsLoading(true);
-        //     setAuth(false);
-        //     save('maxiaSessionToken', '');
-        //     save('email', '');
-        //     save('password', '');
-        //     setUrl(navigator.url);
-        //     return false;
-        // }
-
         setUrl(navigator.url);
         return true;
     }
+
+    const hideSplashScreen = async () => {
+        await SplashScreen.hideAsync();
+    }
+
+    useEffect(() => {
+        setTimeout(() => {
+            if (!started) {
+                console.log("FAILED")
+            }
+        }, 5000);
+        return () => {};
+    }, []);
 
     return (
         <SafeAreaView style={styles.container}>
@@ -64,7 +67,15 @@ export default App = () => {
                 onMessage={({ data }) => { return data }}
                 onLoadEnd={()=>{
                     url.includes("sign_in") ? setStatusBarBackgroundColor("#F2F2F2") : setStatusBarBackgroundColor("white")
+                    if (started < 2) {
+                        setStarted(started+1);
+                    }
+                    if (started == 1) {
+                        hideSplashScreen();
+                        setStarted(started+1);
+                    }
                 }}
+                startInLoadingState
             />
             <StatusBar style="dark" translucent={true} backgroundColor={statusBarBackgroundColor} />
         </SafeAreaView>
